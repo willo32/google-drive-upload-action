ac6c5ee335ab77e6ba3b065449278bf9f0a8499e
Flavien Koehler <flavien@willo.com>
2021-02-23 17:30:41 +0100

    Add action src + prod bundle

---
+++ src/index.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+
+const actions = require('@actions/core');
+const { google } = require('googleapis');
+
+const credentials = actions.getInput('credentials', { required: true });
+const parentFolderId = actions.getInput('parent_folder_id', { required: true });
+const target = actions.getInput('target', { required: true });
+const owner = actions.getInput('owner', { required: false });
+const childFolder = actions.getInput('child_folder', { required: false });
+let filename = actions.getInput('name', { required: false });
+
+const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+const scopes = ['https://www.googleapis.com/auth/drive.file'];
+const auth = new google.auth
+    .JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes, owner);
+const drive = google.drive({ version: 'v3', auth });
+
+async function getUploadFolderId() {
+    if (!childFolder) {
+        return parentFolderId;
+    }
+
+    // Check if child folder already exists and is unique
+    const { data: { files } } = await drive.files.list({
+        q: `name='${childFolder}' and '${parentFolderId}' in parents`,
+        fields: 'files(id)',
+    });
+
+    if (files.length > 1) {
+        throw new Error('More than one entry match the child folder name');
+    }
+    if (files.length === 1) {
+        return files[0].id;
+    }
+
+    const childFolderMetadata = {
+        name: childFolder,
+        mimeType: 'application/vnd.google-apps.folder',
+        parents: [parentFolderId],
+    };
+    const { data: { id: childFolderId } } = await drive.files.create({
+        resource: childFolderMetadata,
+        fields: 'id',
+    });
+
+    return childFolderId;
+}
+
+async function main() {
+    const uploadFolderId = await getUploadFolderId();
+
+    if (!filename) {
+        filename = target.split('/').pop();
+    }
+
+    const fileMetadata = {
+        name: filename,
+        parents: [uploadFolderId],
+    };
+    const fileData = {
+        body: fs.createReadStream(target),
+    };
+
+    return drive.files.create({
+        resource: fileMetadata,
+        media: fileData,
+        uploadType: 'multipart',
+        fields: 'id',
+    });
+}
+
+main().catch((error) => actions.setFailed(error));
